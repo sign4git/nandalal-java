e278967cc84b3036ee55a2d438985844fd683f63
Nandalal <nandalal.1995@hotmail.com>
2024-10-19 17:52:35 +0530

    Job App Using Spring Web

---
+++ JobAppSpringWeb/src/test/java/com/example/JobAppSpringWeb/JobAppSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobAppSpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

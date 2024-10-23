2da271f2170c40a09edac03d41b68bdcfbe1f6d3
Nandalal <nandalal.1995@hotmail.com>
2024-10-23 10:06:05 +0530

    Removed unused imports and formatted code

--- SpringDataRestJobApp/src/main/java/com/example/SpringDataRestJobApp/repository/JobRepository.java
+++ SpringDataRestJobApp/src/main/java/com/example/SpringDataRestJobApp/repository/JobRepository.java
@@ -1,17 +1,11 @@
 package com.example.SpringDataRestJobApp.repository;
 
 
-
-
 import com.example.SpringDataRestJobApp.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface JobRepository extends JpaRepository<JobPost,Integer> {
+public interface JobRepository extends JpaRepository<JobPost, Integer> {
 
 }

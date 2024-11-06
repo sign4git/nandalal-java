df82487abae7ed5f1648de611f242aa841e01b36
Nandalal <nandalal.1995@hotmail.com>
2024-11-06 18:13:54 +0530

    Customized security config

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.SpringSecurity.config;
+
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer->customizer.disable());
+        http.authorizeHttpRequests(customizer-> customizer.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
